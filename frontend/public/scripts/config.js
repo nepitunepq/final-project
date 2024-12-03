2d9b0f2276aea92e90418a8b9a09c7734486082f
Ubuntu <ubuntu@ip-172-31-24-23.ec2.internal>
2024-12-03 12:47:39 +0000

    idk

--- frontend/public/scripts/config.js
+++ frontend/public/scripts/config.js
@@ -1,5 +1,6 @@
 // TODO0: change the BACKEND_URL to your EC2 public IPv4
 export const BACKEND_URL = "http://localhost:3222";
+// export const BACKEND_URL = "http://34.196.173.48:3222";
 
 // TODO0: change element in MEMBERS to your group members.
 // WARNING: the _id parameters is required.
