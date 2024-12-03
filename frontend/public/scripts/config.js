afe2907c13d90481e3f115379d07099c6ffd3840
Ubuntu <ubuntu@ip-172-31-24-23.ec2.internal>
2024-12-03 07:54:10 +0000

    mongo

--- frontend/public/scripts/config.js
+++ frontend/public/scripts/config.js
@@ -1,17 +1,1 @@
-/**
- * @typedef {Object} Item
- * @property {string} _id
- * @property {string} item
- * @property {string} name
- * @property {number} price
- */
-
-/** @typedef {Omit<Item, "_id">} ItemPayload */
-
-export const BACKEND_URL = "http://54.145.247.19:3222";
-
-export const MEMBERS = [
-  "อติวงศ์ สุชาโต",
-  "โปรดปราน บุณยพุกกณะ",
-  "ณัฐวุฒิ หนูไพโรจน์",
-];
+export const BACKEND_URL = "http://3.87.237.240:3222"
